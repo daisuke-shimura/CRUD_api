afdbc07cde85929f25721142d49684c528f0cc57
daisuke-shimura <daisuke.s0707@icloud.com>
2025-06-20 23:07:40 +0000

    show作成

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,11 +3,11 @@ class MemosController < ApplicationController
     memo = Memo.find_by(id: params[:id])
     if memo
       render json: { 
-        "id"        : memo.id,
-        "title"    : memo.title, 
-        "content"  : memo.content, 
-        "createdAt": memo.created_at, 
-        "updatedAt": memo.updated_at 
+        id: memo.id,
+        title: memo.title, 
+        content: memo.content, 
+        created_at: memo.created_at, 
+        updated_at: memo.updated_at,
         }
     else
       render json: { status: 404, message: "Not Found"}, status: 404
@@ -18,6 +18,12 @@ class MemosController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    memo = Meoo.find(params[:id])
+    memo.save!
+    render :show
   end
 
   def update
